0313bdf951a99c57b03e3848abf8f4cabf64740d
SirMengo <55927203+SirMengo@users.noreply.github.com>
2025-04-29 15:48:09 +0100

    Removed unecessary libft files

--- ft_hexputnbr.c
+++ ft_hexputnbr.c
@@ -1,21 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_hexputnbr.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/28 10:30:07 by msimoes           #+#    #+#             */
-/*   Updated: 2025/04/29 15:47:46 by msimoes          ###   ########.fr       */
+/*   Created: 2025/04/29 13:28:17 by msimoes           #+#    #+#             */
+/*   Updated: 2025/04/29 14:59:48 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "libftprintf.h"
 
-int	ft_putchar(int c);
-int	ft_putstr(char *str);
-int	ft_putnbr(long n);
-int	ft_unsputnbr(unsigned int n);
 
-#endif
+int ft_hexputnbr(int n)
+{
+	int i;
+
+	i = ft_unscount(n);
+	if (n >= 10)
+	{
+		ft_hexputnbr(n / 16);
+		ft_hexputnbr(n % 16);
+	}
+	else
+		ft_putchar(n + '0');
+	return (i);
+}
+
+int main()
+{
+	int i = 147483648;
+	printf("%x", i);
+}
