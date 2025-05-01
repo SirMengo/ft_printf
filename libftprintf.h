ae1279cd296673d4e839c0e7c3961a5cf12c75e0
SirMengo <msimoes0103@gmail.com>
2025-05-01 12:32:54 +0100

    Changed name of funcstions for better readability

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 10:30:07 by msimoes           #+#    #+#             */
-/*   Updated: 2025/05/01 12:10:09 by msimoes          ###   ########.fr       */
+/*   Updated: 2025/05/01 12:32:28 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
+# include <stdarg.h>
 
 int	ft_putchar(int c);
 int	ft_putstr(char *str);
 int	ft_putnbr(long n);
-int	ft_unsputnbr(unsigned int n);
-int ft_hexputnbr(unsigned int n, unsigned char c);
+int	ft_unsputuns(unsigned int n);
+int ft_puthex(unsigned int n, unsigned char c);
 
 #endif
